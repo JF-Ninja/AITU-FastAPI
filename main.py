3e517ac9f1e1750b9bde52a51d9a86f5dcdcb1c1
JFN <eserikova22@gmail.com>
2024-11-03 19:14:27 +0500

    111

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ class Registration(BaseModel):
 class VerifyRequest(BaseModel):
     email: str
     verification_code: int
+class ChangePassword(BaseModel):
+    new_password: str
 
 @app.post("/Registration")
 async def Registration(user: Registration):
@@ -149,6 +151,28 @@ async def verify_code(request: VerifyRequest):
         raise HTTPException(status_code=404, detail="Verification code not found for this email")
     if stored_code != request.verification_code:
         raise HTTPException(status_code=400, detail="Incorrect verification code")
-
-    # Успешное подтверждение
+    to_encode = {"login": request.email, "exp": datetime.utcnow() + EXPIRATION_TIME}
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return {"message": "Verification successful"}
+
+@app.post("/change_password")
+async def change_password(request: ChangePassword, token: str = Depends(oauth2_scheme)):
+    conn = None
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("login")
+
+        if not email:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+        conn = await get_database_connection()
+
+        hashed_password = pwd_context.hash(request.new_password)
+
+        query_update_password = """UPDATE users SET password_hash = $1 WHERE user_email = $2;"""
+        row = await conn.execute(query_update_password, hashed_password, email)
+
+        return {"message": "Password updated successfully"}
+    finally:
+        if conn:
+            await conn.close()

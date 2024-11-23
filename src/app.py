e7f3a4916e8ebbcf685aa18fe1ad39e6f0848f74
JFN <eserikova22@gmail.com>
2024-11-23 21:19:25 +0500

    new test

--- src/app.py
+++ src/app.py
@@ -1,6 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routing.auth import router as registration_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(registration_router)

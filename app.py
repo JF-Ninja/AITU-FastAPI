f5ba6ef15347dde6b741c336fef4dbf124e56257
JFN <eserikova22@gmail.com>
2024-11-23 16:24:54 +0500

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from AITU_Project.src.routing.auth import router as registration_router
+from src.routing.auth import router as registration_router
 
 app = FastAPI(openapi_url="/core/openapi.json", docs_url="/core/docs")
 app.include_router(registration_router)

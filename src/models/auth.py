cb608a2292275cbff985359f2f67200a6dd3d691
JFN <eserikova22@gmail.com>
2024-11-24 12:34:27 +0500

    ORM

--- src/models/auth.py
+++ src/models/auth.py
@@ -1,16 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "USERS"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, nullable=False)
-    user_surname = Column(String, nullable=False)
-    user_email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-    user_role = Column(String, nullable=False)
-    gender = Column(String, nullable=False)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String(100), nullable=False)
+    user_surname = Column(String(100), nullable=False)
+    user_email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    profile_image = Column(String(255))
+    user_role = Column(String(50), nullable=False)
+    region = Column(String(255))
+    gender = Column(String(10))
+    created_at = Column(DateTime, default=func.now())

15b6c8d991a74c04ff811e9c4aa6fca594a7e5ed
JFN <eserikova22@gmail.com>
2024-11-24 12:15:55 +0500

    ORM

---
+++ src/models/auth.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, nullable=False)
+    user_surname = Column(String, nullable=False)
+    user_email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    user_role = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -1,15 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
 
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, nullable=False)
-    user_surname = Column(String, nullable=False)
-    user_email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-    user_role = Column(String, nullable=False)
-    gender = Column(String, nullable=False)
+class Registration(BaseModel):
+    name: str
+    surname: str
+    email: str
+    password: str
+    role: str
+    gender: str

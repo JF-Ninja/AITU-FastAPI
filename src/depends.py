04009d3eccf516f35b455d0cae1c829c82cda654
JFN <eserikova22@gmail.com>
2024-11-24 23:31:30 +0500

    test

--- src/depends.py
+++ src/depends.py
@@ -4,7 +4,9 @@ from services.auth import UserService
 from repositories.auth import UserRepository
 from services.token import TokenRepository
 from database import get_db
+from services.email_verification import EmailService
 
 async def get_user_service(db: AsyncSession = Depends(get_db)) -> UserService:
     token_repository = TokenRepository()
-    return UserService(UserRepository(db), token_repository)
+    email_service = EmailService
+    return UserService(UserRepository(db), token_repository, email_service)

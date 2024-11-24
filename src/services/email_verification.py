1fd2b6d4999e99290e1034f0358cf801f20f057a
JFN <eserikova22@gmail.com>
2024-11-25 00:47:29 +0500

    test

--- src/services/email_verification.py
+++ src/services/email_verification.py
@@ -19,12 +19,13 @@ class EmailService:
         text = f"Ваш код верификации: {verification_code}"
         msg.attach(MIMEText(text, "plain"))
         try:
-            print(999)
             async with aiosmtplib.SMTP(hostname=EmailService.smtp_server, port=EmailService.port, timeout=10) as smtp:
+                print(111)
                 await smtp.connect()
+                print(222)
                 await smtp.login(EmailService.sender_email, EmailService.sender_password)
+                print(333)
                 await smtp.sendmail(EmailService.sender_email, to_email, msg.as_string())
-                print(9999)
         except Exception as e:
             print(f"Error sending email: {e}")
 
